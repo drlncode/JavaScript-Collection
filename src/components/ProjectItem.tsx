aea8ee63af952d3344a76b8398a8de7d0489d0f1
Darlin Daniel Arias M. <darlin.code@gmail.com>
2024-11-15 20:39:49 -0400

    add: projects page content (5/?).

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -2,7 +2,7 @@ import { Project } from '@/types/projects';
 
 export default function ProjectItem({ project }: { project: Project }) {
     return (
-        <div className='w-64 p-2 mb-1 border border-main-yellow/40 rounded-lg transition-colors hover:border-main-yellow/80 hover:shadow-project'>
+        <div className='group w-64 p-2 mb-1 border border-main-yellow/40 rounded-lg duration-200 transition-colors hover:border-main-yellow/80 hover:shadow-project relative overflow-hidden'>
             <div className='flex flex-col'>
                 <div className='flex justify-center items-center max-w-60'>
                     <div className='flex justify-center items-center h-[130px] w-full overflow-hidden rounded-md border border-main-yellow/40'>
@@ -13,8 +13,16 @@ export default function ProjectItem({ project }: { project: Project }) {
                         />
                     </div>
                 </div>
-                <div></div>
+                <div className='flex flex-col font-montserrat-300 pl-0.5 pb-1'>
+                    <div className='text-xl py-1'>
+                        <h2>{ project.id }</h2>
+                    </div>
+                    <div className='text-xs font-extralight'>
+                        <p>{ project.description }</p>
+                    </div>
+                </div>
             </div>
+            <div className='invisible select-none content:visible content:select-auto absolute duration-200 transition-colors bg-main-black/40 w-full h-full top-0 left-0 group-hover:bg-transparent'></div>
         </div>
     );
 }
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,7 +4,7 @@ import ProjectItem from '@/components/ProjectItem';
 export default function ProjectsPage() {
     return (
         <section className='px-1.5'>
-            <div className='flex justify-center gap-1.5 flex-wrap pt-8 content:justify-between'>
+            <div className='flex justify-center gap-3 content:gap-1.5 flex-wrap pt-8 content:justify-between'>
                 {(projects.map(project => (
                     <ProjectItem project={project} key={project.id} />
                 )))}
